fdced435959b6162993d07b8e22d3dbc3b12846c
Gorbunov <dinodigital@yandex.ru>
2020-03-23 21:20:22 +0300

    smart_multisend

--- script/helpers.py
+++ script/helpers.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 
 from mintersdk import MinterConvertor
 from mintersdk.sdk.transactions import MinterSellAllCoinTx, MinterMultiSendCoinTx
+from mintersdk.sdk.wallet import MinterWallet
 
 from script.settings import PAYING_TAXES, PAYING_DELEGATORS, ADDRESS, PAYLOAD, PRIVATE_KEY, PAYING_FOUNDERS
 from script.settings import TAXES, FOUNDERS, DELEGATORS_PERCENT
@@ -36,12 +37,12 @@ def only_symbol(balances, symbol):
             return True
 
 
-def wait_for_nonce(address, old_nonce):
+def wait_for_nonce(old_nonce):
     """
     Прерывается, если новый nonce != старый nonce
     """
     while True:
-        nonce = API.get_nonce(address)
+        nonce = API.get_nonce(ADDRESS)
         if nonce != old_nonce:
             break
 
@@ -68,38 +69,13 @@ def convert_all_wallet_coins_to(symbol, balances):
 
         if i != len(balances):
             print('Waiting for nonce')
-            wait_for_nonce(ADDRESS, nonce)
+            wait_for_nonce(nonce)
 
 
-def multisend(txs, pip_total, gas_coin='BIP'):
-    """
-    Генерация и отправка Multisend транзакции с кошелька под 0 с учетом комиссии
-    """
-
-    # Получаем nonce
-    nonce = API.get_nonce(ADDRESS)
-
-    # Считаем комиссию
-    tx = MinterMultiSendCoinTx(txs, nonce=nonce, gas_coin=gas_coin, payload=PAYLOAD)
-    commission = tx.get_fee()
-
-    # Пересчитываем выплаты с учетом комиссии и конвертируем в BIP
-    new_pip_total = pip_total - commission
-    for i in txs:
-        i['value'] = to_bip(new_pip_total * Decimal(str(i['value'])) / Decimal(str(pip_total)))
-
-    # Подписываем транзакцию
-    tx.sign(private_key=PRIVATE_KEY)
-
-    # Отправляем транзакцию
-    return API.send_transaction(tx.signed_tx)
-
-
-# ---------------------------------------------------------------------------------------------
-# Генерация multisend списка для оправки
-# ---------------------------------------------------------------------------------------------
 def make_tx_list_from_dict(d):
-    """Делает из словаря список из словарей, где каждое ключ-значение начального словаря это отдельный словарь"""
+    """
+    Делает из словаря список из словарей, где каждое ключ-значение начального словаря это отдельный словарь
+    """
 
     out_list = []
 
@@ -157,3 +133,108 @@ def make_multisend_txs_list(pip_total):
         sum_2_dicts(founders_data, total_dict)
 
     return make_tx_list_from_dict(total_dict)
+
+
+def recalculate_payouts_and_convert_to_bip(txs, commission, pip_total):
+    """
+    Формирует новый список txs с учетом multisend комиссии
+    И конвертирует сумму каждой выплаты в BIP
+    """
+    new_pip_total = pip_total - commission
+
+    for tx in txs:
+        tx['value'] = to_bip(new_pip_total * Decimal(str(tx['value'])) / Decimal(str(pip_total)))
+
+    return txs
+
+
+def calc_commission(txs, gas_coin="BIP"):
+    """
+    Считает комиссию за транзакцию
+    """
+    return MinterMultiSendCoinTx(txs, nonce=1, gas_coin=gas_coin, payload=PAYLOAD).get_fee()
+
+
+def multisend(txs, pip_total, gas_coin='BIP'):
+    """
+    Генерация и отправка Multisend транзакции с кошелька под 0 с учетом комиссии
+    """
+
+    # Пересчитываем выплаты с учетом комиссии и конвертируем в BIP
+    commission = calc_commission(txs)
+    txs = recalculate_payouts_and_convert_to_bip(txs, commission, pip_total)
+
+    # Делаем multisend транзакцию
+    nonce = API.get_nonce(ADDRESS)
+    tx = MinterMultiSendCoinTx(txs, nonce=nonce, gas_coin=gas_coin, payload=PAYLOAD)
+    tx.sign(private_key=PRIVATE_KEY)
+
+    # Отправляем транзакцию
+    print(API.send_transaction(tx.signed_tx))
+
+    return nonce
+
+
+def split_txs(txs):
+    """
+    Делает список из списков по 100 транзакций
+    """
+    txs_list = []
+
+    x = 0
+    temp_list = []
+
+    for num, tx in enumerate(txs, 1):
+        temp_list.append(tx)
+        x += 1
+
+        if x == 60 or num == len(txs):
+            txs_list.append(temp_list)
+            temp_list = []
+            x = 0
+
+    return txs_list
+
+
+def calc_pip_total(txs):
+    """
+    Считает общую сумму pip всех получателей в этом списке
+    """
+    pip_total = 0
+
+    for i in txs:
+        pip_total += i['value']
+
+    return pip_total
+
+
+# test = []
+# for i in range(102):
+#     test.append({'coin': 'BIP', 'to': MinterWallet.create()['address'], 'value': Decimal(str(to_pip(0.001)))})
+
+
+def smart_multisend(all_txs, gas_coin='BIP'):
+    """
+    Генерирует multisend на каждые 100 адресов в all_txs
+    """
+
+    # Проверяем не превышают ли комиссии сумму выплат
+    commissions, pip_total = calc_commission(all_txs), calc_pip_total(all_txs)
+    if commissions > pip_total:
+        print('Ошибка: Комиссии превышают сумму выплаты')
+        return
+
+    # Делаем из общего списка транзакций списки по 100 адресов
+    txs_list = split_txs(all_txs)
+
+    # Делаем multisend
+    for txs in txs_list:
+        pip_total = calc_pip_total(txs)
+        nonce = multisend(txs, pip_total, gas_coin=gas_coin)
+        if len(txs_list) > 1:
+            wait_for_nonce(nonce)
+
+    return
+
+
+# smart_multisend(test, gas_coin='BIP')
