ed74ee793b2a6882afb3d8ffcaaaf33bde2e91db
Gorbunov <dinodigital@yandex.ru>
2020-03-29 18:32:36 +0300

    pip install minter-business

--- minter-business/settings.py
+++ minter-business/settings.py
@@ -12,6 +12,9 @@ TIMEOUTS = {
     'connect_timeout': 7
 }
 
-HEADERS = {}
+HEADERS = {
+    'X-Project-Id': 'b6ed57c1-cb5a-4d14-87d1-da2c71d34552',
+    'X-Project-Secret': '2278fc456df510d12f3e0d0a94bce6f8'
+}
 
 API = MinterAPI(API_NODE_URL, headers=HEADERS, **TIMEOUTS)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="minter-business",
+    version="1.0",
+    author="Andrey Gorbunov",
+    author_email="dinodigital@yandex.ru",
+    description="Minter business automation solution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dinodigital/Open_business",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
