ec65a9fa6cd8893cbae51298096ff87798371573
Gorbunov <dinodigital@yandex.ru>
2020-03-30 22:25:14 +0300

    install requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="minterbiz",
-    version="1.0.0",
+    version="1.0.1",
     author="Andrey Gorbunov",
     author_email="dinodigital@yandex.ru",
     description="Minter business constructor",
@@ -19,4 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'mintersdk'
+    ]
 )
